57cf8a507680291ea53cfb4111706c2ac492a507
Pleopleq <felix.anducho74@gmail.com>
2021-07-23 11:00:14 -0500

    Update mock and add JWT as a dependency

--- mocks/products.js
+++ mocks/products.js
@@ -1,45 +1,57 @@
 let PRODUCTS = [
-    {
+    {   
+        id: 1,
         name: "Left 4 Dead",
         description: "De la mano de Valve®, creadores de Counter-Strike®, Half-Life® y otros muchos juegos, llega Left 4 Dead™, una nueva aventura para PC y Xbox 360 que te meterá en la piel de uno de los cuatro supervivientes que libran una pugna de proporciones épicas contra hordas de zombis y sus terroríficas variedades mutantes especiales.",
         cover_image: "https://cdn.cloudflare.steamstatic.com/steam/apps/500/header.jpg?t=1623087651",
         price: 19.99,
-        stock: 10,    
+        stock: 10,
+        in_cart: false,  
     },
     {
+        id: 2,
         name: "Sid Meier's Civilization VI",
         description: "Sid Meier's Civilization VI o Civilization VI es un videojuego de estrategia por turnos perteneciente a la serie Civilization. El juego fue desarrollado por Firaxis Games y distribuido por 2K Games y Take-Two Interactive.",
         cover_image: "https://cdn.cloudflare.steamstatic.com/steam/apps/289070/header_spanish.jpg?t=1626210772",
         price: 39.99,
         stock: 10,
+        in_cart: false,
     },
     {
+        id: 3,
         name: "Stellaris",
         description: "Explora una galaxia repleta de maravillas con este juego de ciencia ficción y gran estrategia de Paradox Development Studio. Interactúa con distintas especies alienígenas, descubre nuevos mundos y amplía el dominio de tu imperio.",
         cover_image: "https://cdn.cloudflare.steamstatic.com/steam/apps/281990/header.jpg?t=1621840092",
         price: 39.99,
         stock: 20,
+        in_cart: false,
     },
     {
+        id: 4,
         name: "RimWorld",
         description: "Un simulador de Colonia de Ciencia Ficción creada por una brillante Inteligencia Artificial que narrará la historia. Genera historias al simular psicología, ecología, combate, clima, biomas, diplomacia, relaciones interpersonales, arte, medicina, comercio y mucho más",
         cover_image: "https://cdn.cloudflare.steamstatic.com/steam/apps/294100/header.jpg?t=1624985273",
         price: 34.99,
-        stock: 20
+        stock: 20,
+        in_cart: false,
     },
     {
+        id: 5,
         name: "Cities: Skylines",
         description: "Introduce nuevos elementos a la mecánica de juego para plasmar la emoción y las complicaciones de mantener una ciudad de verdad, ampliando al mismo tiempo algunos de los temas ya conocidos de la experiencia del desarrollo urbanístico.",
         cover_image: "https://cdn.cloudflare.steamstatic.com/steam/apps/255710/header.jpg?t=1621621159",
         price: 29.99,
-        stock: 5
+        stock: 5,
+        in_cart: false,
     },
     {
+        id: 6,
         name: "The Binding of Isaac",
         description: "Cuando la madre de Isaac empieza a escuchar la voz de Dios exigiéndole un sacrificio para demostrar su fe, Isaac huye hacia el sótano para enfrentarse a montones de desquiciados enemigos, hermanos y hermanas perdidos, sus miedos y, finalmente, a su madre.",
         cover_image: "https://cdn.cloudflare.steamstatic.com/steam/apps/113200/header.jpg?t=1573195411",
         price: 4.99,
-        stock: 20
+        stock: 20,
+        in_cart: false,
     }
 ]
 
